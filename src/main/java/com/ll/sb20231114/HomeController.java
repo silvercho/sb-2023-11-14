3da0a3c6008c35b3e6dd3a2ca5b7618bb4b86ea3
silvercho <tjddms01212@gmail.com>
2023-11-14 13:12:11 +0900

    calc 13

--- src/main/java/com/ll/sb20231114/HomeController.java
+++ src/main/java/com/ll/sb20231114/HomeController.java
@@ -1,9 +1,15 @@
 package com.ll.sb20231114;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class HomeController {
@@ -20,17 +26,17 @@ public class HomeController {
         return "개발자 커뮤니티";
     }
 
-    @GetMapping ("/clac")
+    @GetMapping ("/calc")
     @ResponseBody
     String showCalc(int a , int b){
         return "계산 결과 : %d".formatted(a+b);
     }
-    @GetMapping ("/clac2")
+    @GetMapping ("/calc2")
     @ResponseBody
     String showCalc2(Integer a , Integer b){
         return "a : " + a + " , b : " + b;
     }
-    @GetMapping ("/clac3")
+    @GetMapping ("/calc3")
     @ResponseBody
     String showCalc3(
             @RequestParam(defaultValue =  "0") int a,
@@ -40,7 +46,7 @@ public class HomeController {
     }
     // http://localhost:8020/clac3?a=10&b=20
 
-    @GetMapping ("/clac4")
+    @GetMapping ("/calc4")
     @ResponseBody
     String showCalc4(
             @RequestParam(defaultValue =  "0") double a,
@@ -49,12 +55,100 @@ public class HomeController {
         return "계산 결과 : %d".formatted(a+b);
     }
 
-    @GetMapping ("/clac5")
+    @GetMapping ("/calc5")
     @ResponseBody
     String showCalc5(
-            @RequestParam(defaultValue =  "0") String a,
-            @RequestParam(defaultValue =  "0") String b
-    ){
-        return "계산 결과 : %d".formatted(a+b);
+            @RequestParam(defaultValue = "-") String a,
+            @RequestParam(defaultValue = "-") String b
+    ) {
+        return "계산 결과 : %s".formatted(a + b);
     }
+    @GetMapping("/calc6")
+    @ResponseBody
+    int showCalc6(
+            int a, int b
+    ) {
+        return a + b;
+    }
+
+    @GetMapping("/calc7")
+    @ResponseBody
+    boolean showCalc7(
+            int a, int b
+    ) {
+        return a > b;
+    }
+    @GetMapping("/calc8")
+    @ResponseBody
+    Person showCalc8(
+            String name, int age
+    ) {
+        return new Person(name, age);
+    }
+
+    @GetMapping("/calc9")
+    @ResponseBody
+    Person2 showCalc9(
+            String name, int age
+    ) {
+        return new Person2(name, age);
+    }
+    @GetMapping("/calc10")
+    @ResponseBody
+    Map<String, Object> showCalc10(
+            String name, int age
+    ) {
+        Map<String, Object> personMap = Map.of(
+                "name", name,
+                "age", age
+        );
+
+        return personMap;
+    }
+
+    @GetMapping("/calc11")
+    @ResponseBody
+    List<Integer> showCalc11() {
+        List<Integer> nums = new ArrayList<>() {{
+            add(10);
+            add(-510);
+            add(10010);
+        }};
+
+        return nums;
+    }
+    @GetMapping("/calc12")
+    @ResponseBody
+    int[] showCalc12() {
+        int[] nums = new int[]{10, -510, 10010};
+
+        return nums;
+    }
+
+    @GetMapping("/calc13")
+    @ResponseBody
+    List<Person2> showCalc13(
+            String name, int age
+    ) {
+        List<Person2> persons = new ArrayList<>() {{
+            add(new Person2(name, age));
+            add(new Person2(name + "!", age + 1));
+            add(new Person2(name + "!!", age + 2));
+        }};
+
+        return persons;
+    }
+}
+@AllArgsConstructor // 생성자 자동 생성
+class Person {
+    public String name;
+    public int age;
+}
+
+@AllArgsConstructor
+class Person2 {
+    @Getter
+    private String name;
+    @Getter
+    private int age;
 }
