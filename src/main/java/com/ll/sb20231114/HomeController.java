bf5041d7189e1afeac45cb203619946ceded52f2
silvercho <tjddms01212@gmail.com>
2023-11-14 10:55:20 +0900

    clac5

--- src/main/java/com/ll/sb20231114/HomeController.java
+++ src/main/java/com/ll/sb20231114/HomeController.java
@@ -2,6 +2,7 @@ package com.ll.sb20231114;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -17,5 +18,43 @@ public class HomeController {
     @ResponseBody
     String showAbout(){
         return "개발자 커뮤니티";
+    }
+
+    @GetMapping ("/clac")
+    @ResponseBody
+    String showCalc(int a , int b){
+        return "계산 결과 : %d".formatted(a+b);
+    }
+    @GetMapping ("/clac2")
+    @ResponseBody
+    String showCalc2(Integer a , Integer b){
+        return "a : " + a + " , b : " + b;
+    }
+    @GetMapping ("/clac3")
+    @ResponseBody
+    String showCalc3(
+            @RequestParam(defaultValue =  "0") int a,
+            @RequestParam(defaultValue =  "0") int b
+    ){
+        return "계산 결과 : %d".formatted(a+b);
+    }
+    // http://localhost:8020/clac3?a=10&b=20
+
+    @GetMapping ("/clac4")
+    @ResponseBody
+    String showCalc4(
+            @RequestParam(defaultValue =  "0") double a,
+            @RequestParam(defaultValue =  "0") double b
+    ){
+        return "계산 결과 : %d".formatted(a+b);
+    }
+
+    @GetMapping ("/clac5")
+    @ResponseBody
+    String showCalc5(
+            @RequestParam(defaultValue =  "0") String a,
+            @RequestParam(defaultValue =  "0") String b
+    ){
+        return "계산 결과 : %d".formatted(a+b);
     }
 }
