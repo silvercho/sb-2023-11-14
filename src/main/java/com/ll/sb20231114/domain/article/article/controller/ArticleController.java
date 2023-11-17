9c2e467e4750274eacf8846783c031d1fe54186e
silvercho <tjddms01212@gmail.com>
2023-11-17 18:19:56 +0900

    45

--- src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.ll.sb20231114.domain.article.article.controller;
 import com.ll.sb20231114.domain.article.article.entity.Article;
 import com.ll.sb20231114.domain.article.article.service.ArticleService;
 import com.ll.sb20231114.global.rsData.RsData;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,6 +37,25 @@ public class ArticleController {
 
         return rs;
     }
+    @PostMapping("/article/write2")
+    @ResponseBody
+    RsData write2(
+            HttpServletRequest req
+    ) {
+        String title = req.getParameter("title");
+        String body = req.getParameter("body");
+
+        Article article = articleService.write(title, body);
+
+        RsData<Article> rs = new RsData<>(
+                "S-1",
+                "%d번 게시물이 작성되었습니다.".formatted(article.getId()),
+                article
+        );
+
+        return rs;
+    }
+
     @GetMapping ("article/getLastArticle")
     @ResponseBody
     Article getLastArticle(){
