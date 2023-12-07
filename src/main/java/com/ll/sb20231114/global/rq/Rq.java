d386465fe08747eb12ea7b4040c335f9d42250b1
silvercho <tjddms01212@gmail.com>
2023-12-07 14:08:21 +0900

    97-5

--- src/main/java/com/ll/sb20231114/global/rq/Rq.java
+++ src/main/java/com/ll/sb20231114/global/rq/Rq.java
@@ -2,53 +2,61 @@ package com.ll.sb20231114.global.rq;
 
 import com.ll.sb20231114.domain.member.member.entity.Member;
 import com.ll.sb20231114.domain.member.member.service.MemberService;
+import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.security.Security;
 import java.util.Optional;
 
 @RequestScope // HTTP 요청마다 새로운 빈 인스턴스가 생성
 @Component
 @Getter
+@RequiredArgsConstructor
 public class Rq {
     private final HttpServletRequest req;
     private final HttpServletResponse resp;
     private final MemberService memberService;
+    private User user;
     private Member member;
 
-    public Rq(HttpServletRequest req, HttpServletResponse resp, MemberService memberService) {
-        this.req = req;
-        this.resp = resp;
-        this.memberService = memberService;
-    }
+    @PostConstruct
+    public void init() {
+        //현재 로그인 한 회원의 인증정보를 가져옴
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
+        if (authentication.getPrincipal() instanceof User) {
+            this.user = (User) authentication.getPrincipal();
+        }
+    }
     public String redirect(String path, String msg) {
         msg = URLEncoder.encode(msg, StandardCharsets.UTF_8);
 
         return "redirect:" + path + "?msg=" + msg;
     }
 
-    private long getMemberId() {
-        return Optional
-                .ofNullable(req.getSession().getAttribute("loginedMemberId"))
-                .map(id -> (long) id)
-                .orElse(0L);
+    private String getMemberUsername() {
+        return user.getUsername();
     }
 
     public boolean isLogined() {
-            return getMemberId() > 0;
+            return user != null;
         }
     public Member getMember(){
         if (!isLogined()){
             return null;
         }
         if (member == null)
-            member = memberService.findById(getMemberId()).get();
+            member = memberService.findByUsername(getMemberUsername()).get();
 
         return member;
     }
@@ -65,6 +73,8 @@ public class Rq {
     }
 
     public boolean isAdmin() {
-        return getMember().isAdmin();
+        return user.getAuthorities()
+                .stream()
+                .anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"));
     }
 }
--- src/main/java/com/ll/sb20231114/global/security/SecurityConfig.java
+++ src/main/java/com/ll/sb20231114/global/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.ll.sb20231114.global.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,7 +24,7 @@ public class SecurityConfig {
                 .build();
     }
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+    PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
     }
 }
